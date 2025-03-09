4057af86fbde0572cb865bb2512b72742506d3e0
harivansh <harivanshbhardwaj57@gmail.com>
2025-03-10 01:49:39 +0530

    changed typo

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./component/Header";
 import Home from "./component/Home";
 import "./styles/App.scss";
 import "./styles/Home.scss";
-import "./styles/Header.scss";
+import "./styles/header.scss";
 import "./styles/Footer.scss";
 import "./styles/Contact.scss";
 import "./styles/mediaquery.scss";
@@ -14,16 +14,17 @@ import Contact from "./component/Contact";
 import Services from "./component/Services";
 
 const App = () => {
-  return (<>
-    <Router>
-      <Header/>
-      <Routes>
-        <Route path="/" element ={<Home/>} />
-        <Route path="/contact" element ={<Contact/>} />
-        <Route path="/services" element ={<Services/>} />
-      </Routes>
-      <Footer/>
-    </Router>
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 };
